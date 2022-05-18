f027f73c36ede665e0ef1a161e9c350dccce4037
verrrooon <verrrooon@yandex.ru>
2022-05-18 18:56:55 +0300

    дополнения

--- src/contacts.js
+++ src/contacts.js
@@ -1,26 +1,38 @@
-const contacts = document.getElementsByClassName("contacts")[0];
-const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
+const contacts = document.getElementsByClassName('contacts')[0];
+const stickyHeader = document.getElementsByClassName('stickyHeader')[0];
+
+function throttle(func, timeout) {
+  let timer = null;
+
+  return function perform(...args) {
+    if (timer) return;
+
+    timer = setTimeout(() => {
+      func(...args);
+      clearTimeout(timer);
+      timer = null;
+    }, timeout);
+  };
+}
 
 function addContacts() {
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < 50000; i++) {
-    const child = document.createElement("div");
+    const child = document.createElement('div');
     child.textContent = i;
-    child.classList.add("contact");
+    child.classList.add('contact');
     fragment.appendChild(child);
   }
   contacts.appendChild(fragment);
 }
 
-contacts.addEventListener("scroll", (e) => {
-  const items = Array.from(contacts.getElementsByClassName("contact"));
-  const itemOffsets = items.map((item) => item.offsetTop);
-  const topItemIndex = itemOffsets.findIndex(
-    (offset) => contacts.scrollTop - offset <= -18
-  );
-  if (topItemIndex !== -1) {
-    stickyHeader.textContent = items[topItemIndex].textContent;
-  }
-});
-
 addContacts();
+
+contacts.addEventListener(
+  'scroll',
+  throttle((e) => {
+    const contact = contacts.getElementsByClassName('contact')[0];
+    console.log(contacts.scrollTop, contact.offsetTop);
+    stickyHeader.textContent = Math.floor(contacts.scrollTop / contact.offsetTop);
+  }, 250),
+);
